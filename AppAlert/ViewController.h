9aa1d749f3c6cf8543d3d877a84014489223f4c3
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-20 17:37:08 -0600

    App alert

--- AppAlert/ViewController.h
+++ AppAlert/ViewController.h
@@ -9,6 +9,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *etiqueta;
+- (IBAction)actionSheet:(id)sender;
 
 @end
 
--- AppAlert/ViewController.m
+++ AppAlert/ViewController.m
@@ -19,4 +19,25 @@
 }
 
 
+- (IBAction)actionSheet:(id)sender {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Titulo" message:@"Mensaje" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction* action1 = [UIAlertAction actionWithTitle:@"Boton 1" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        self.etiqueta.text = @"Boton 1 fue presionado";
+    }];
+    
+    
+    UIAlertAction* action2 = [UIAlertAction actionWithTitle:@"Boton 2" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        self.etiqueta.text = @"Boton 2 fue presionado";
+    }];
+    
+    UIAlertAction* cancel = [UIAlertAction actionWithTitle:@"Dismiss" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        [alert dismissViewControllerAnimated:YES completion:nil];
+    }];
+    
+    [alert addAction:action1];
+    [alert addAction:action2];
+    [alert addAction:cancel];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 @end
